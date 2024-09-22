1ee6b7f31d3cf130bef08460c808132a8721c2c7
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2024-09-23 01:04:14 +0800

    Calculator v1.0
    A fully functional simple calculator

---
+++ src/main.ts
@@ -0,0 +1,146 @@
+const Display = document.querySelector<HTMLInputElement>("#Display")
+const History = document.querySelector<HTMLInputElement>("#History")
+
+//toggle for when equal is entered, resets it to zero when a new digit is clicked.
+let input = false;
+//toggle stores the display value
+let history: any = "";
+
+//Resets the Calculator
+const AC = document.querySelector<HTMLButtonElement>("#AC")
+AC?.addEventListener("click", () => {
+    if (Display) Display.value = "0";
+    if (History) History.value = "";
+    Enabled();
+    Display?.classList.remove("background");
+    History?.classList.remove("background");
+    Display?.classList.remove("textanimation");
+    clearTimeout(1000);
+});;
+
+//Stores the id of the numbers in an array
+const Buttons = ["b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "Dec"];
+Buttons.forEach(num => { //goes through the array
+    const button = document.querySelector<HTMLButtonElement>(`#${num}`) //queries over the document id to search for the button  
+    button?.addEventListener("click", () => {  //check if specific button is clicked it displays its value
+        if (input) {
+            Display!.value = "0";
+            input=false;
+        }
+        if ( Display && Display!.value.length < 16) {
+            if (Display!.value === "0") {
+                if (num === "Dec") {
+                    if (!Display.value.includes(".")) {
+                        Display!.value += "."; //adds the decimal instead of replacing the 0
+                    }
+                } else {
+                    Display!.value = num[1];
+                console.log("hi")
+                }
+            } else {
+                if (num === "Dec") {
+                    if (!Display.value.includes(".")) {
+                        Display!.value += "."; //prevents from having two decimals
+                    } 
+                } else Display!.value += num[1];
+            }
+        }
+        history = Display!.value;
+    })
+});
+
+//takes the last index of the text and deletes it
+const Del = document.querySelector<HTMLButtonElement>("#Del")
+Del?.addEventListener("click", () => {
+    if (Display!.value.length > 1) Display!.value = `${Display?.value.slice(0, -1)}`;
+    else Display!.value = "0";
+});
+
+//id of operations are stored in an array
+const Operations = ["Add", "Sub", "Div", "Multiply"]
+Operations.forEach(signs => {
+    const Operaters = document.querySelector<HTMLButtonElement>(`#${signs}`) //goes over them and quqeries over them in the document
+    Operaters?.addEventListener("click", () => { //check displays the operation when clicked, on screen.
+            const lastchar = Display!.value.charAt(Display!.value.length -1 )
+            const check = ["+", "−", "÷", "×"].includes(lastchar)
+            if (!check && Display!.value.length < 16) {
+                if (signs === "Add") {
+                    Display!.value += "+";
+                }
+                if (signs === "Sub") {
+                    Display!.value += "−";
+                }
+                if (signs === "Div") {
+                    Display!.value += "÷";
+                }
+                if (signs === "Multiply") Display!.value += "×";
+            }
+        })
+    })
+
+//performs the solving
+const Enter = document.querySelector<HTMLButtonElement>("#Equal")
+Enter?.addEventListener("click", () => { 
+    const solved = history.replace("−", "-").replace("÷", "/").replace("×", "*")
+    Display!.value = `${eval(solved)}`.slice(0,16);
+    History!.value = `${history} = ${Display!.value}`;
+    input = true;
+});
+
+//says the goodbye message on screen
+const Bye = document.querySelector<HTMLButtonElement>("#Bye")
+ Bye?.addEventListener("click", ()=> {
+    Display!.value = "Sasageyoo!";
+    Display?.classList.add("textanimation");
+    setTimeout(Off, 2000);
+    Disabled();
+ });
+
+
+function Off() {
+    History?.classList.add("background");
+    Display?.classList.add("background"); 
+    if (AC) {AC.disabled = false};
+};
+
+
+//Disables all Button
+function Disabled() {
+    Operations.forEach(signs => {
+        const Operaters = document.querySelector<HTMLButtonElement>(`#${signs}`) //goes over them and quqeries over them in the document
+        if (Operaters) Operaters.disabled = true;
+    });
+    Buttons.forEach(num => {
+        const button = document.querySelector<HTMLButtonElement>(`#${num}`)
+        if (button) button.disabled = true;
+    });
+
+    if (Hello) Hello.disabled = true;
+    if (AC) AC.disabled = true;
+    if (Enter) Enter.disabled = true;
+    if (Del) Del.disabled = true;
+}
+
+//Enables all Button
+function Enabled() {
+    
+    Operations.forEach(signs => {
+        const Operaters = document.querySelector<HTMLButtonElement>(`#${signs}`) //goes over them and quqeries over them in the document
+        if (Operaters) Operaters.disabled = false;
+    })
+    Buttons.forEach(num => {
+        const button = document.querySelector<HTMLButtonElement>(`#${num}`)
+        if (button) button.disabled = false;
+    })
+    
+    if (AC) AC.disabled = true;
+    if (Hello) Hello.disabled = true;
+    if (Enter) Enter.disabled = false;
+    if (Del) Del.disabled = false;
+}
+
+const Hello = document.querySelector<HTMLButtonElement>("#Hello")
+Hello?.addEventListener("click", () => {
+    const greetings = ["Hola", "Kamusta", "Konichiwa", "Ciao", "Salaam", "Namaste", "Hallo", "Bonjour"]
+    Display!.value = greetings[Math.floor(Math.random() * 8)];
+});
